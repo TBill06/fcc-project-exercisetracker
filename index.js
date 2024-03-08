62e4e28b6233ba068886e9a1cbcce658423cffd7
TBill06 <bill.tushar2@gmail.com>
2024-03-08 20:07:27 -0330

    FCC-4

--- index.js
+++ index.js
@@ -1,18 +1,148 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 require('dotenv').config()
 
-app.use(cors())
+//Basic Configuration
+const port = process.env.PORT || 3000;
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri)
+  .then(() => console.log('Connected to MongoDB successfully'))
+  .catch((err) => console.log('Error connecting to MongoDB', err));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+//Database Schema Configuration
+const exerciseSchema = new mongoose.Schema( {
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: String,
+}, { _id: false });
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  log: [exerciseSchema]
+});
+const user = mongoose.model('user', userSchema);
 
+//POST user
+app.post('/api/users', (req, res) => {
+  const username = req.body.username;
+  checkUserName(username).then((data) => {
+    if(data) {
+      res.json({ error: 'Username already exists' });
+    }
+    else {
+      user.create({ username: username }).then((data) => {
+        res.json({ username: data.username, _id: data._id });
+      })
+    }
+  })
+});
 
+//GET all users
+app.get('/api/users', (req, res) => {
+  user.find({})
+  .then((data) => {
+    res.json({ username: data.username, _id: data._id });
+  })
+  .catch((err) => {
+    res.json({ error: err });
+  })
+});
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+//Check if username exists
+function checkUserName(username) {
+  return user.findOne({ username: username }).then((data) => {
+    if (data) {
+      console.log('Username already exists');
+      return true;
+    }
+    else {
+      console.log('Username does not exist');
+      return false;
+    }
+  });
+}
+
+//POST exercises
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const _id = req.params._id;
+  const description = req.body.description;
+  const duration = req.body.duration;
+  let date = req.body.date;
+  
+  if (!date) {
+    date = new Date().toISOString().substring(0, 10);
+  }
+  user.findById(_id).then((data) => {
+    if (!data) {
+      res.json({ error: 'User not found' });
+    }
+    else {
+      let exercise = { description: description, duration: duration, date: date };
+      let logExists = data.log.some((log) => (log.date === date && log.description === description && String(log.duration) === duration));
+      if (logExists) {
+        res.json({ error: 'Exercise already logged' });
+      }
+      else {
+        data.log.push(exercise);
+        data.save().then((updatedUser) => {
+          res.json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            description: description,
+            duration: duration,
+            date: date
+          });
+        })
+        .catch((err) => {
+          res.json({ error: err });
+        });
+      }
+    }
+  });
+});
+
+//GET Logs
+app.get('/api/users/:_id/logs', (req, res) => {
+  const _id = req.params._id;
+  const from = req.query.from;
+  const to = req.query.to;
+  const limit = req.query.limit;
+  user.findById(_id).then((data) => {
+    if (!data) {
+      res.json({ error: 'User not found' });
+    }
+    else {
+      let log = data.log
+      if (from) {
+        log = log.filter((log) => new Date(log.date) >= new Date(from));
+      }
+      if (to) {
+        log = log.filter((log) => new Date(log.date) <= new Date(to));
+      }
+      if (limit) {
+        log = log.slice(0, limit);
+      }
+      res.json({
+        _id: data._id,
+        username: data.username,
+        count: data.log.length,
+        log: log
+      });
+    }
+  });
+});
+
+const listener = app.listen(port, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
